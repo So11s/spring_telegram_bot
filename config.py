48c351e55ee5e6344d672d01f5bd1a4665410eae
Egor Sidorov <lelikegor1@gmail.com>
2023-04-22 15:28:16 +0300

    add fucntion to get weather of city

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
+# api key for geodata
 geo_key: str = "fdbc1e07-9e4b-4589-9b8f-291164b01ff6"
+
+# api key for weather data
+weather_key: dict = {"X-Yandex-API-Key": "c22db718-fcf9-4e73-9eb2-a9382316dd88"}
--- request.py
+++ request.py
@@ -2,7 +2,8 @@ import requests
 import config
 
 
-def get_city(city: str) -> requests:
+def get_city_coord(city: str) -> str:
+    # get geo data of city
     payload: dict = {
         "geocode": city,
         "apikey": config.geo_key,
@@ -10,8 +11,15 @@ def get_city(city: str) -> requests:
         }
     url: str = "https://geocode-maps.yandex.ru/1.x"
     response = requests.get(url=url, params=payload)
-    geo_data = response.json()
-    return geo_data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+    geo_data: dict = response.json()
+    return geo_data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
 
 
-print(get_city("Moscow"))
+def get_weather(city: str) -> str:
+    # get weather data of city
+    coordinates: list = get_city_coord(city).split()
+    payload: dict = {"lat": coordinates[1], "lon": coordinates[0], "lang": "ru_RU"}
+    url: str = "https://api.weather.yandex.ru/v2/forecast"
+    response = requests.get(url=url, params=payload, headers=config.weather_key)
+    weather: dict = response.json()
+    return weather["fact"]
