baeb65c11cb8bec7d343fc354e280d3ac56decb7
Egor Sidorov <lelikegor1@gmail.com>
2023-04-22 15:51:21 +0300

    create directories for architecture project

--- api_config/config.py
+++
@@ -1,5 +0,0 @@
-# api for geo data
-geo_key: str = "fdbc1e07-9e4b-4589-9b8f-291164b01ff6"
-
-# api for weather data
-weather_key: str = "c22db718-fcf9-4e73-9eb2-a9382316dd88"
---
+++ api_requests/request.py
@@ -0,0 +1,25 @@
+import requests
+from settings import config
+
+
+def get_city_coord(city: str) -> str:
+    # get geo data of city
+    payload: dict = {
+        "geocode": city,
+        "apikey": config.geo_key,
+        "format": "json",
+        }
+    url: str = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(url=url, params=payload)
+    geo_data: dict = response.json()
+    return geo_data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
+
+
+def get_weather(city: str) -> str:
+    # get weather data of city
+    coordinates: list = get_city_coord(city).split()
+    payload: dict = {"lat": coordinates[1], "lon": coordinates[0], "lang": "ru_RU"}
+    url: str = "https://api.weather.yandex.ru/v2/forecast"
+    response = requests.get(url=url, params=payload, headers=config.weather_key)
+    weather: dict = response.json()
+    return weather
